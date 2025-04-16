c3ea816494562c5ebecb50cd060d1a8c96109969
markoMedved <marko.medved2002@gmail.com>
2025-04-16 18:02:53 +0200

    finished first part

--- Advanced-tracking/particle_filter_tracker.py
+++ Advanced-tracking/particle_filter_tracker.py
@@ -12,7 +12,7 @@ class ParticleFilterTracker(Tracker):
             x_ = np.array(region[::2])
             y_ = np.array(region[1::2])
             region = [np.min(x_), np.min(y_), np.max(x_) - np.min(x_) + 1, np.max(y_) - np.min(y_) + 1]
-
+    
         
 
     def track(self, image):
